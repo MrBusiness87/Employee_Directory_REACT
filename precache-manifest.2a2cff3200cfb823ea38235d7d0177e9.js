c0569c8e5deee4939964ec7b70c83f1b05b3fed8
MrBusiness87 <bbartula87@gmail.com>
2020-10-06 18:06:02 -0400

    Updates

---
+++ precache-manifest.2a2cff3200cfb823ea38235d7d0177e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f990c0fad5929c076d8cb081e8745f",
+    "url": "/Employee_Directory_REACT/index.html"
+  },
+  {
+    "revision": "cc1d96c270682529682b",
+    "url": "/Employee_Directory_REACT/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "cc1d96c270682529682b",
+    "url": "/Employee_Directory_REACT/static/js/2.9eeb7b25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee_Directory_REACT/static/js/2.9eeb7b25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b133d722b974a50bf863",
+    "url": "/Employee_Directory_REACT/static/js/main.7aa82b81.chunk.js"
+  },
+  {
+    "revision": "7b2327a57a9395fb9599",
+    "url": "/Employee_Directory_REACT/static/js/runtime-main.75055269.js"
+  }
+]);
